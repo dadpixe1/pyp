761dcdaf641951b8b28a28067fdb6b9bcc7514e3
story <antipriest@mail.com>
2022-12-17 19:08:15 +0300

    to do

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -17,7 +17,32 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
+y = 500
+x = 100`
+
+y2 = 450
+x2 = 150
+while True:
+    sd.clear_screen()
+    point = sd.get_point(x, y)
+    sd.snowflake(center=point, length=50)
+    y -= 10
+    if y < 50:
+       break
+    x = x + 10
+
+    point2 = sd.get_point(x2, y2)
+    sd.snowflake(center=point2, length=30)
+    y2 -= 10
+    if y2 < 50:
+       break
+    x2 = x2 + 20
+
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
+
+
 while True:
     sd.clear_screen()
     pass
