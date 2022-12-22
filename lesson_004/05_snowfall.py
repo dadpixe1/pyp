8252117a461714b11fd5ed203b1b28591194f750
story <antipriest@mail.com>
2022-12-22 13:13:51 +0300

    done

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -15,41 +15,58 @@ N = 20
 # sd.sleep()
 # sd.random_number()
 # sd.user_want_exit()
+user_input = input('Укажите интенсивность снегопада (от 1 до 20): ')
 x_list = []
 y_list = []
 rays_length_list = []
-wind = sd.random_number(a=2, b=10)
+factor_a_list = []
+factor_b_list = []
+factor_c_list = []
+wind_list = []
+snowdrift_list = []
 
 
-while count < N + 1:
+while count < N + N:
     count += 1
     x_list.append(sd.random_number(a=10, b=590))
     y_list.append(sd.random_number(a=550, b=600))
-    rays_length_list.append(sd.random_number(a=10, b=30))
+    rays_length_list.append(sd.random_number(a=10, b=15))
+    factor_a_list.append((sd.random_number(a=1, b=10) / 10))
+    factor_b_list.append((sd.random_number(a=1, b=100) / 100))
+    factor_c_list.append(sd.random_number(a=1, b=179))
+    wind_list.append(sd.random_number(a=-10, b=10))
+    snowdrift_list.append(sd.random_number(a=15, b=45))
 
 
-def hz(x):
+def snowflake(x):
     for i in range(x):
         sd.start_drawing()
         point = sd.get_point(x=x_list[i], y=y_list[i])
         length = rays_length_list[i]
-        sd.snowflake(center=point, length=length, color=sd.background_color)
-        y_list[i] -= 40
-        x_list[i] += sd.random_number(a=-2, b=6)
-
-            # sd.snowflake(center=point, length=length, color=sd.COLOR_WHITE)
-        new_point = sd.get_point(x=x_list[i], y=y_list[i])
-        sd.snowflake(center=new_point, length=length, color=sd.COLOR_WHITE)
-        if y_list[0] < 25:
-            sd.snowflake(center=point, length=length, color=sd.COLOR_WHITE)
+        factor_a, factor_b, factor_c = factor_a_list[i], factor_b_list[i], factor_c_list[i]
+        sd.snowflake(center=point, length=length, color=sd.background_color,
+                     factor_a=factor_a, factor_b=factor_b, factor_c=factor_c)
+        y_list[i] -= 35
+        x_list[i] += wind_list[sd.random_number(a=0, b=19)]
+        if y_list[i] < 45:
+            last_point = sd.get_point(x=x_list[i], y=snowdrift_list[i])
+            sd.snowflake(center=last_point, length=length, color=sd.COLOR_WHITE,
+                         factor_a=factor_a, factor_b=factor_b, factor_c=factor_c)
+            y_list[i] = sd.random_number(a=550, b=600)
+            x_list[i] = sd.random_number(a=10, b=590)
+            factor_a_list[i], factor_b_list[i], factor_c_list[i] = (sd.random_number(a=1, b=10) / 10),\
+                (sd.random_number(a=1, b=100) / 100), (sd.random_number(a=1, b=179))
             continue
+        new_point = sd.get_point(x=x_list[i], y=y_list[i])
+        sd.snowflake(center=new_point, length=length, color=sd.COLOR_WHITE,
+                     factor_a=factor_a, factor_b=factor_b, factor_c=factor_c)
         sd.finish_drawing()
         sd.sleep(0.1)
 
 
 while True:
-    for _ in range(N + 1):
-        hz(x=_)
+    for _ in range(int(user_input) + 1):
+        snowflake(x=_)
     if sd.user_want_exit():
         break
 
