4fa36a874f180874330717f10084770c09685287
story <antipriest@mail.com>
2023-01-31 19:19:53 +0300

    Done. Little fixes. Ё-fix по желанию.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -28,17 +28,17 @@ from random import randint
 
 class AlphaStat:
     """ Подсчет статистики по буквам в тексте. Параметр file_name - имя и расширение файла в кавычках.
-    Сортировка при alfabet=True - по старшинству алфавита,
-    при alfabet=False - с конца алфавита, при alfabet='i' - переход в режим сортировки значений increase.
-    Режим increase: При increase=True - по возрастанию числа букв, increase=False - по убыванию. """
+    Сортировка: sort_by='A' - по старшинству алфавита, при sort_by='Z' - с конца алфавита,
+    sort_by=9 - по возрастанию числа букв, sort_by=1 - по убыванию. """
 
-    def __init__(self, file_name, alfabet, increase):
+    def __init__(self, file_name, sort_by):
+        self.column_width = 0
+        self.sort_by = sort_by
         self.total_value = 0
-        self.increase = increase
-        self.alfabet = alfabet
         self.file_name = file_name
         self.stat = {}
-        self.stat_cyrillic = {}
+        self.cyrillic = {}
+        self.sorted_list = []
         self.collect()
 
     def unzip(self):
@@ -67,47 +67,48 @@ class AlphaStat:
             encoding[ord(symbol)] = value
         for code, value in encoding.items():
             if code in range(1000, 1200):
-                self.stat_cyrillic[chr(code)] = value
+                self.total_value += value
+                self.cyrillic[chr(code)] = value
+        self.column_width = len(str(self.total_value)) + 4
         self.frame()
 
     def frame(self):
-        column_width = len(str(self.total_value)) + 4
-        column_width, column1_sign, column2_sign, end_sign = column_width, 'буква', 'частота', 'итого'
-        edge = '{plus}{line}{plus}{line}{plus}'.format(plus='+', line='-' * column_width)
-        head = f'|{column1_sign: ^{column_width}}|{column2_sign: ^{column_width}}|'
-        print(edge)
-        print(head)
-        print(edge)
-        self.method_choosing(column_width=column_width)
-        end = f'|{end_sign: ^{column_width}}|{self.total_value: ^{column_width}}|'
-        print(edge)
-        print(end)
-        print(edge)
-
-    def method_choosing(self, column_width):
-        sorted_list = []
-        if self.alfabet == 'i':
-            sorted_tuple = sorted(self.stat_cyrillic, key=self.stat_cyrillic.get, reverse=self.increase)
-            for key in sorted_tuple:
-                value = key, self.stat_cyrillic[key]
-                sorted_list.append(value)
-        if sorted_list:
-            for letter, value in sorted_list:
-                self.total_value += value
-                print(f'|{letter: ^{column_width}}|{value: ^{column_width}}|')
-        elif not self.alfabet:
-            for letter in sorted(self.stat_cyrillic, reverse=True):
-                self.total_value += self.stat_cyrillic[letter]
-                print(f'|{letter: ^{column_width}}|{self.stat_cyrillic[letter]: ^{column_width}}|')
+        match_list = [1, 9, 'A', 'Z']
+        if self.sort_by not in match_list:
+            print('Sort_by error. You must enter int 1 or 9 for frequency or A, Z for lettering.')
         else:
-            for letter, value in sorted(self.stat_cyrillic.items()):
-                self.total_value += value
-                print(f'|{letter: ^{column_width}}|{value: ^{column_width}}|')
+            column_width, column1_sign, column2_sign, end_sign = self.column_width, 'буква', 'частота', 'итого'
+            edge = '{plus}{line}{plus}{line}{plus}'.format(plus='+', line='-' * column_width)
+            head = f'|{column1_sign: ^{column_width}}|{column2_sign: ^{column_width}}|'
+            print(f'{edge}\n{head}\n{edge}')
+            self.method_choosing()
+            end = f'|{end_sign: ^{column_width}}|{self.total_value: ^{column_width}}|'
+            print(f'{edge}\n{end}\n{edge}')
+
+    def method_choosing(self):
+        if self.sort_by == 1:
+            self.sort_list(reverse=False)
+            for letter, value in self.sorted_list:
+                print(f'|{letter: ^{self.column_width}}|{value: ^{self.column_width}}|')
+        elif self.sort_by == 9:
+            self.sort_list(reverse=True)
+            for letter, value in self.sorted_list:
+                print(f'|{letter: ^{self.column_width}}|{value: ^{self.column_width}}|')
+        elif self.sort_by == 'Z':
+            for letter in sorted(self.cyrillic, reverse=True):
+                print(f'|{letter: ^{self.column_width}}|{self.cyrillic[letter]: ^{self.column_width}}|')
+        elif self.sort_by == 'A':
+            for letter, value in sorted(self.cyrillic.items()):
+                print(f'|{letter: ^{self.column_width}}|{value: ^{self.column_width}}|')
+
+    def sort_list(self, reverse):
+        sorted_tuple = sorted(self.cyrillic, key=self.cyrillic.get, reverse=reverse)
+        for key in sorted_tuple:
+            value = key, self.cyrillic[key]
+            self.sorted_list.append(value)
 
 
-AlphaStat(file_name='voyna-i-mir.txt.zip', alfabet=True, increase=True)
-
-
+AlphaStat(file_name='voyna-i-mir.txt.zip', sort_by='y')
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
