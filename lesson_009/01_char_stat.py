a985b3974909ec23bf27e5c6969958bc4f0488d8
story <antipriest@mail.com>
2023-01-31 15:12:51 +0300

    Done. Позиционирование буквы Ё по желанию.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -26,12 +26,20 @@ from pprint import pprint
 from random import randint
 
 
-class Chatterer:
+class AlphaStat:
+    """ Подсчет статистики по буквам в тексте. Параметр file_name - имя и расширение файла в кавычках.
+    Сортировка при alfabet=True - по старшинству алфавита,
+    при alfabet=False - с конца алфавита, при alfabet='i' - переход в режим сортировки значений increase.
+    Режим increase: При increase=True - по возрастанию числа букв, increase=False - по убыванию. """
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, alfabet, increase):
+        self.total_value = 0
+        self.increase = increase
+        self.alfabet = alfabet
         self.file_name = file_name
         self.stat = {}
-
+        self.stat_cyrillic = {}
+        self.collect()
 
     def unzip(self):
         zfile = zipfile.ZipFile(self.file_name, 'r')
@@ -50,66 +58,55 @@ class Chatterer:
                             self.stat[char] += 1
                         else:
                             self.stat[char] = 1
-        pprint(self.stat)
+        self.sort()
+
+    def sort(self):
+        encoding = {}
+        for symbol, value in self.stat.items():
+            self.total_value += value
+            encoding[ord(symbol)] = value
+        for code, value in encoding.items():
+            if code in range(1000, 1200):
+                self.stat_cyrillic[chr(code)] = value
+        self.frame()
+
+    def frame(self):
+        column_width = len(str(self.total_value)) + 4
+        column_width, column1_sign, column2_sign, end_sign = column_width, 'буква', 'частота', 'итого'
+        edge = '{plus}{line}{plus}{line}{plus}'.format(plus='+', line='-' * column_width)
+        head = f'|{column1_sign: ^{column_width}}|{column2_sign: ^{column_width}}|'
+        print(edge)
+        print(head)
+        print(edge)
+        self.method_choosing(column_width=column_width)
+        end = f'|{end_sign: ^{column_width}}|{self.total_value: ^{column_width}}|'
+        print(edge)
+        print(end)
+        print(edge)
+
+    def method_choosing(self, column_width):
+        sorted_list = []
+        if self.alfabet == 'i':
+            sorted_tuple = sorted(self.stat_cyrillic, key=self.stat_cyrillic.get, reverse=self.increase)
+            for key in sorted_tuple:
+                value = key, self.stat_cyrillic[key]
+                sorted_list.append(value)
+        if sorted_list:
+            for letter, value in sorted_list:
+                self.total_value += value
+                print(f'|{letter: ^{column_width}}|{value: ^{column_width}}|')
+        elif not self.alfabet:
+            for letter in sorted(self.stat_cyrillic, reverse=True):
+                self.total_value += self.stat_cyrillic[letter]
+                print(f'|{letter: ^{column_width}}|{self.stat_cyrillic[letter]: ^{column_width}}|')
+        else:
+            for letter, value in sorted(self.stat_cyrillic.items()):
+                self.total_value += value
+                print(f'|{letter: ^{column_width}}|{value: ^{column_width}}|')
 
 
+AlphaStat(file_name='voyna-i-mir.txt.zip', alfabet=True, increase=True)
 
-
-
-    # def prepare(self):
-    #     self.totals = {}
-    #     self.stat_for_generate = {}
-    #     for sequence, char_stat in self.stat.items():
-    #         self.totals[sequence] = 0
-    #         self.stat_for_generate[sequence] = []
-    #         for char, count in char_stat.items():
-    #             self.totals[sequence] += count
-    #             self.stat_for_generate[sequence].append([count, char])
-    #             self.stat_for_generate[sequence].sort(reverse=True)
-    #
-    # def chat(self, N, out_file_name=None):
-    #     N = 1000
-    #     printed = 0
-    #     if out_file_name is not None:
-    #         file = open(out_file_name, 'w', encoding='utf8')
-    #     else:
-    #         file = None
-    #
-    #     sequence = ' ' * self.analize_count
-    #     spaces_printed = 0
-    #     while printed < N:
-    #         char = self._get_char(char_stat=self.stat_for_generate[sequence], total=self.totals[sequence])
-    #         if file:
-    #             file.write(char)
-    #         else:
-    #             print(char, end='')
-    #         if char == ' ':
-    #             spaces_printed += 1
-    #             if spaces_printed >= 10:
-    #                 if file:
-    #                     file.write('\n')
-    #                 else:
-    #                     print()
-    #                 spaces_printed = 0
-    #         printed += 1
-    #         sequence = sequence[1:] + char
-    #     if file:
-    #         file.close()
-    #
-    # def _get_char(self, char_stat, total):
-    #     dice = randint(1, total)
-    #     pos = 0
-    #     for count, char in char_stat:
-    #         pos += count
-    #         if dice <= pos:
-    #             break
-    #     return char
-
-
-chatterer = Chatterer(file_name='voyna-i-mir.txt.zip')
-chatterer.collect()
-# chatterer.prepare()
-# chatterer.chat(N=10000, out_file_name='out.txt')
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
