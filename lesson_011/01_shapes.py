205c0b7fb0b57d90ff45e176ccaa52d157cf8a37
story <antipriest@mail.com>
2023-02-06 12:26:59 +0300

    Done

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -14,12 +14,21 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    def figure(point=sd.get_point(350, 350), angle=0, length=200):
+        sides = list(range(3, 12))
+        if angle > 361:
+            return
+        value = [360 / x for x in sides if x == n]
+        side = sd.get_vector(start_point=point, angle=angle, length=length, width=2)
+        side.draw()
+        next_point = side.end_point
+        next_angle = angle + value[0]
+        return figure(point=next_point, angle=next_angle, length=length)
+    return figure
 
 
-draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
+draw_triangle = get_polygon(n=2)
+draw_triangle(point=sd.get_point(200, 200), angle=33, length=50)
 
 
 sd.pause()
