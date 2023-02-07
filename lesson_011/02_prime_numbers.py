225caab9ef52154fd74c3fec45541ddf70519383
story <antipriest@mail.com>
2023-02-07 20:14:19 +0300

    ShbiTcodin

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -4,15 +4,15 @@
 # Есть функция генерации списка простых чисел
 
 
-# def get_prime_numbers(n):
-#     prime_numbers = []
-#     for number in range(2, n+1):
-#         for prime in prime_numbers:
-#             if number % prime == 0:
-#                 break
-#         else:
-#             prime_numbers.append(number)
-#     return prime_numbers
+def get_prime_numbers(n):
+    prime_numbers = []
+    for number in range(2, n + 1):
+        for prime in prime_numbers:
+            if number % prime == 0:
+                break
+        else:
+            prime_numbers.append(number)
+    return prime_numbers
 
 
 # Часть 1
@@ -22,40 +22,46 @@
 # Распечатать все простые числа до 10000 в столбик
 
 
-class PrimeNumbers:
+# class PrimeNumbers:
+#
+#     def __init__(self, n):
+#         self.n = n
+#         self.prime_numbers = []
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         for number in range(2, self.n + 1):
+#             if number == self.n:
+#                 raise StopIteration()
+#             for prime in self.prime_numbers:
+#                 if number % prime == 0:
+#                     break
+#             else:
+#                 self.prime_numbers.append(number)
+#                 return number
+#
+#
+# prime_number_iterator = PrimeNumbers(n=1000)
+# for number in prime_number_iterator:
+#     print(number)
 
-    def __init__(self, n):
-        self.n = n
-        self.prime_numbers = []
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        for number in range(2, self.n + 1):
-            if number == self.n:
-                raise StopIteration()
-            for prime in self.prime_numbers:
-                if number % prime == 0:
-                    break
-            else:
-                self.prime_numbers.append(number)
-                return number
-
-
-prime_number_iterator = PrimeNumbers(n=1000)
-for number in prime_number_iterator:
-    print(number)
-
-# TODO после подтверждения части 1 преподователем, можно делать
 # Часть 2
 # Теперь нужно создать генератор, который выдает последовательность простых чисел до n
 # Распечатать все простые числа до 10000 в столбик
 
 
 # def prime_numbers_generator(n):
-#     pass
-#     # TODO здесь ваш код
+#     prime_numbers = []
+#     for number in range(2, n + 1):
+#         for prime in prime_numbers:
+#             if number % prime == 0:
+#                 break
+#         else:
+#             prime_numbers.append(number)
+#             yield number
 #
 #
 # for number in prime_numbers_generator(n=10000):
@@ -77,3 +83,19 @@ for number in prime_number_iterator:
 # простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
 #
 # Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
+
+# def lucky_numbers(n):
+#     numbers, half = [], int(len(str(n)) / 2)
+#     for numb in str(n):
+#         numbers.append(numb)
+#     result_l = sum(int(x) for x in numbers[half:] if len(str(n)) % 2 == 0)
+#     result_r = sum(int(x) for x in numbers[:half] if len(str(n)) % 2 == 0)
+#     res_l, res_r = sum(int(x) for x in numbers[half + 1:]), sum(int(x) for x in numbers[:half + 1])
+#     if res_r == res_l or result_r == result_l:
+#         return True
+#
+#
+# print(lucky_numbers(92083))
+
+
+
