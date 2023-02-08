e57771c7853c3ba8053c2ffc083bf41e242f61d5
story <antipriest@mail.com>
2023-02-08 13:15:15 +0300

    Done

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from pprint import pprint
+import zipfile
 # На основе своего кода из lesson_009/02_log_parser.py напишите итератор (или генератор)
 # котрый читает исходный файл events.txt и выдает число событий NOK за каждую минуту
 # <время> <число повторений>
@@ -14,4 +16,23 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+def log_collect(file_name):
+    log, count = [], 0
+    with open(file_name, 'r', encoding='cp1251') as file:
+        for line in file:
+            if 'NOK' in line:
+                item = line.split(' ')
+                data, time = item[0][1::], item[1][0: 5]
+                log.append(f'{data} {time}')
+                if len(log) > 1:
+                    count += 1
+                    if log[-2] != log[-1]:
+                        yield log[-2], count
+                        count = 0
+
+
+grouped_events = log_collect('events.txt')
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
+
