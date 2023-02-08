9e641206b425a14cd6bd4fd596bc5d37a7ef0275
story <antipriest@mail.com>
2023-02-08 18:59:16 +0300

    Partly done

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -9,41 +9,51 @@
 
 
 def log_errors(func):
-    pass
-    # TODO здесь ваш код
+    def surrogate(param):
+        try:
+            result = func(param)
+            print(result)
+        except ArithmeticError as exc:
+            print(f'Hey, we got {type(exc)} here in {func.__name__}`s {param}: {exc}')
+            with open('function_errors.log', 'a', encoding='utf8') as file:
+                file.write(f'Hey, we got {type(exc)} here in {func.__name__}`s attribute "{param}": {exc}\n')
+    return surrogate
+
 
 
 # Проверить работу на следующих функциях
-@log_errors
-def perky(param):
-    return param / 0
+# @log_errors
+# def perky(param):
+#     return param / 0
+#
+# perky(9j)
 
 
-@log_errors
-def check_line(line):
-    name, email, age = line.split(' ')
-    if not name.isalpha():
-        raise ValueError("it's not a name")
-    if '@' not in email or '.' not in email:
-        raise ValueError("it's not a email")
-    if not 10 <= int(age) <= 99:
-        raise ValueError('Age not in 10..99 range')
-
-
-lines = [
-    'Ярослав bxh@ya.ru 600',
-    'Земфира tslzp@mail.ru 52',
-    'Тролль nsocnzas.mail.ru 82',
-    'Джигурда wqxq@gmail.com 29',
-    'Земфира 86',
-    'Равшан wmsuuzsxi@mail.ru 35',
-]
-for line in lines:
-    try:
-        check_line(line)
-    except Exception as exc:
-        print(f'Invalid format: {exc}')
-perky(param=42)
+# @log_errors
+# def check_line(line):
+#     name, email, age = line.split(' ')
+#     if not name.isalpha():
+#         raise ValueError("it's not a name")
+#     if '@' not in email or '.' not in email:
+#         raise ValueError("it's not a email")
+#     if not 10 <= int(age) <= 99:
+#         raise ValueError('Age not in 10..99 range')
+#
+#
+# lines = [
+#     'Ярослав bxh@ya.ru 600',
+#     'Земфира tslzp@mail.ru 52',
+#     'Тролль nsocnzas.mail.ru 82',
+#     'Джигурда wqxq@gmail.com 29',
+#     'Земфира 86',
+#     'Равшан wmsuuzsxi@mail.ru 35',
+# ]
+# for line in lines:
+#     try:
+#         check_line(line)
+#     except Exception as exc:
+#         print(f'Invalid format: {exc}')
+# perky(param=42)
 
 
 # Усложненное задание (делать по желанию).
